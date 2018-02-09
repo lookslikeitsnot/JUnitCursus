e0c81ff5a4bc5248c326760b04e7d05b37a405c7
cedric.bingen <cedric.bingen@OntCur-0E180363.vdabheverlee.intra>
2018-02-09 12:17:37 +0100

    met fixtures

--- src/test/java/be/vdab/entities/VeilingTest.java
+++ src/test/java/be/vdab/entities/VeilingTest.java
@@ -2,25 +2,29 @@ package be.vdab.entities;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class VeilingTest {
+	private Veiling veiling;
+	@Before
+	public void before() {
+		veiling = new Veiling();
+	}
+	
 	@Test
 	public void hoogsteBod_zonderBod_isNul() {
-		Veiling veiling = new Veiling();
 		assertEquals(0, veiling.getHoogsteBod());
 	}
 	
 	@Test
 	public void hoogsteBod_metEenBod_isHetBod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(1000);
 		assertEquals(1000, veiling.getHoogsteBod());
 	}
 	
 	@Test
 	public void hoogsteBod_metBiedingen_isHoogsteBod() {
-		Veiling veiling = new Veiling();
 		veiling.doeBod(1000);
 		veiling.doeBod(2000);
 		veiling.doeBod(500);
